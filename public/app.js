cc02e7e92c460dd28ba40e7ef66d3f4919ee9164
Russ James <66641621+Russjames92@users.noreply.github.com>
2026-01-04 21:06:05 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -48,10 +48,10 @@ const btnEndBannerClose = document.getElementById("btnEndBannerClose");
 const sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
 // End-game media (served from /public)
-const victoryAudio = new Audio("./trumpet-blast.mp3");
-const defeatAudio  = new Audio("./defeat-audio.mp3");
-const victoryBannerSrc = "./imgs/vic-banner.png";
-const defeatBannerSrc  = "./imgs/def-banner.png";
+const victoryAudio = new Audio("./public/trumpet-blast.mp3");
+const defeatAudio  = new Audio("./public/defeat-audio.mp3");
+const victoryBannerSrc = "./public/imgs/vic-banner.png";
+const defeatBannerSrc  = "./public/imgs/def-banner.png";
 
 let endBannerShown = false;
 function hideEndBanner(){
